5eeed2318cca437dd9d4216b3815429080686a67
Alexander Mami <algru81@gmail.com>
2021-11-18 02:24:48 +0300

    load on startup and url mapping , dispatcher servlet and register it with the servlet context object. setconfiglocation(xml file)

--- spring-love-calculator/src/main/java/com/mami/lc/config/LoveCalculatorApplicationInitializer.java
+++ spring-love-calculator/src/main/java/com/mami/lc/config/LoveCalculatorApplicationInitializer.java
@@ -1,9 +1,12 @@
 package com.mami.lc.config;
 
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.XmlWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 import java.util.logging.Logger;
 
 public class LoveCalculatorApplicationInitializer implements WebApplicationInitializer {
@@ -11,8 +14,31 @@ public class LoveCalculatorApplicationInitializer implements WebApplicationIniti
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
 
-       Logger logger = Logger.getLogger(LoveCalculatorApplicationInitializer.class.getName());
-       logger.info("\n === > message from onStartup method");
+
+        Logger logger = Logger.getLogger(LoveCalculatorApplicationInitializer.class.getName());
+        logger.info("\n =======================================> message from onStartup method");
+
+
+        XmlWebApplicationContext webApplicationContext = new XmlWebApplicationContext();
+        webApplicationContext.setConfigLocations("classpath:/app-config.xml");
+
+        //create a dispatcher servlet object
+
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(webApplicationContext);
+
+        // register the dispatcher servlet with yhe servlet context object
+
+        ServletRegistration.Dynamic myDispatcherServlet = servletContext.addServlet("myDispatcherServlet", dispatcherServlet);
+
+        // load on startup like in xml
+
+        myDispatcherServlet.setLoadOnStartup(1);
+
+        //url mapping
+        myDispatcherServlet.addMapping("/mywebsite.com/*");
+
+
+
 
     }
 }
