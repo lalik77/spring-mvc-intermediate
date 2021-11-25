1b6cf6901ee21a3e5055f06b16f8450ca844ebf6
Alexander Mami <algru81@gmail.com>
2021-11-25 19:12:14 +0300

    Correct some errors

--- spring-love-calculator/src/main/java/com/mami/lc/validator/PhoneValidatorConstraints.java
+++ spring-love-calculator/src/main/java/com/mami/lc/validator/PhoneValidatorConstraints.java
@@ -10,35 +10,25 @@ public class PhoneValidatorConstraints implements ConstraintValidator<VPhone, Co
     @Override
     public void initialize(VPhone constraintAnnotation) {
 
-
-
     }
 
     @Override
     public boolean isValid(CommunicationDTO communicationDTO, ConstraintValidatorContext constraintValidatorContext) {
 
-
         if (communicationDTO.getPhone() == null) {
             return false;
         }
 
-        //isDigitEntered(String country, String base)
-       boolean isDigit = isDigitInput(communicationDTO.getPhone().getCountryCode(),
+        boolean isDigit = isDigitInput(communicationDTO.getPhone().getCountryCode(),
                 communicationDTO.getPhone().getBaseNumber());
 
-        if(!isDigit) {
-
-            return false;
-        }
+        if(!isDigit) {  return false;    }
 
         int lengthB = communicationDTO.getPhone().getBaseNumber().length();
         int lengthC = communicationDTO.getPhone().getCountryCode().length();
         int sum = lengthB + lengthC;
 
-        if (sum!=10) {
-            return false;
-        }
-
+        if (sum!=10) {   return false;       }
 
         return true;
     }
@@ -46,6 +36,6 @@ public class PhoneValidatorConstraints implements ConstraintValidator<VPhone, Co
     private boolean isDigitInput(String countryCode, String baseNumber) {
 
         return countryCode.matches("[0-9]+")
-                && countryCode.matches("[0-9]+");
+                && baseNumber.matches("[0-9]+");
     }
 }
