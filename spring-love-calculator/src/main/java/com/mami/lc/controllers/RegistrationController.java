89ccb3672f07124b317824d66ca44935a6c1a3bd
Alexander Mami <algru81@gmail.com>
2021-11-30 16:57:07 +0300

    Add init binder method for logging purposes

--- spring-love-calculator/src/main/java/com/mami/lc/controllers/RegistrationController.java
+++ spring-love-calculator/src/main/java/com/mami/lc/controllers/RegistrationController.java
@@ -6,6 +6,7 @@ import com.mami.lc.api.UserRegistrationDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -67,5 +68,12 @@ public class RegistrationController {
 
 
         return "reg-success";
+    }
+
+
+    @InitBinder
+    public void initBinder(){
+
+        System.out.println("===================>Inside init binder");
     }
 }
