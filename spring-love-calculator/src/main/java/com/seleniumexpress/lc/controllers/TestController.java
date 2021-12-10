0d40d4944d156d7ed7963e07efd87861c9317990
Alexander Mami <algru81@gmail.com>
2021-12-10 14:49:15 +0300

    Custom editor example finished for date and for number provided by Spring

--- spring-love-calculator/src/main/java/com/seleniumexpress/lc/controllers/TestController.java
+++ spring-love-calculator/src/main/java/com/seleniumexpress/lc/controllers/TestController.java
@@ -1,13 +1,20 @@
 package com.seleniumexpress.lc.controllers;
 
+import com.seleniumexpress.test.api.BillDTO;
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import javax.validation.Valid;
-
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.beans.propertyeditors.CustomNumberEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.seleniumexpress.test.api.BillDTO;
 
 @Controller
 public class TestController {
@@ -20,7 +27,8 @@ public class TestController {
 	}
 
 	@RequestMapping(value = "/process-bill")
-	public String showResultPage(@Valid @ModelAttribute("billDTO") BillDTO billDTO, BindingResult result) {
+	public String showResultPage(@Valid @ModelAttribute("billDTO") BillDTO billDTO,
+			BindingResult result) {
 
 		if (result.hasErrors()) {
 
@@ -28,6 +36,21 @@ public class TestController {
 		}
 
 		return "result-page";
+
+	}
+
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+		CustomDateEditor customDateEditor = new CustomDateEditor(dateFormat, true);
+		binder.registerCustomEditor(Date.class, "date", customDateEditor);
+
+		NumberFormat numberFormat = new DecimalFormat("#,###.00");
+		CustomNumberEditor customNumberEditor = new CustomNumberEditor(BigDecimal.class, numberFormat,
+				true);
+		binder.registerCustomEditor(BigDecimal.class,"amount",customNumberEditor);
+
 
 	}
 }
